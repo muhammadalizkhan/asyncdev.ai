10adc98835ded51d8a1f33f6708ffc2530f89733
Muhamad Ali Khan <your_username@users.noreply.github.com>
2024-11-25 00:43:54 +0500

    style: develop serivce and pricing ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,13 @@
+import Navbar from "./components/NavBar/Navbar";
 import Hero from "./Hero/Page";
+import Pricing from "./Pricing/Page";
+import Services from "./Services/page";
 export default function Page() {
   return (
     <div>
       <Hero />
+      <Services />
+      <Pricing />
     </div>
   );
 }
