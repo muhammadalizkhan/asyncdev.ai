64a85fae112633b934f5fc11fa2a9a75e4cf3d1d
Muhamad Ali Khan <ali7847khan@gmail.com>
2025-01-25 19:04:59 +0500

    Updating UI for Home && Components

--- src/app/Services/page.tsx
+++ src/app/Services/page.tsx
@@ -1,26 +1,11 @@
-import React from 'react';
-import './Services.css';
-import ServiceComponent from '../components/ServiceComponent/ServiceComponent';
-import ExploreOurTech from '../components/ExploreOurTech/ExploreOurTech'
-import TrustedPartner from '../components/TrustedPartner/TrustedPartner';
+import React from "react";
+import "./Services.css";
+import ServiceComponent from "../components/ServiceComponent/ServiceComponent";
 
 export default function Services() {
   return (
     <div className="services-container">
-      <div className="header-section">
-        <h1>Custom Software Development Services</h1>
-        <p>
-          Power your business with robust, secure, and scalable software solutions. AsynDev is a leading custom
-          software development company with a proven track record of delivering exceptional value.
-          AsynDev is a leading custom
-          software development company with a proven track record of delivering exceptional value.
-          AsynDev is a leading custom
-          software development company with a proven track record of delivering exceptional value.
-        </p>
-        <button className="build-btn">Let's Build Something</button>
-      </div>
       <ServiceComponent />
-      <TrustedPartner />
     </div>
   );
 }
