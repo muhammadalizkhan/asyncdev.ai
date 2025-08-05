aaac2e5f8bb7280987af5a62ddc56644204417c0
Muhamad Ali Khan <ali7847khan@gmail.com>
2025-08-05 09:19:49 +0500

    feat custom software development

--- src/app/Services/custom-software-development/page.tsx
+++ src/app/Services/custom-software-development/page.tsx
@@ -3,13 +3,15 @@ import HeroSection from './heroSection'
 import ServicesOffers from './servicesSection'
 import FaqSection from './fqaSection'
 import DevelopmentSection from './developmentProcessApproach'
+import ContactUs from '@/app/components/ContactUs/ContactUs'
 export default function page() {
   return (
     <>
       <HeroSection />
+            <DevelopmentSection />
       <ServicesOffers />
-      <DevelopmentSection />
       <FaqSection />
+      <ContactUs />
     </>
   )
 }
